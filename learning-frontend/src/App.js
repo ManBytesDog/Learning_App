c735470aae76b9125fcd4c2f9d96009934c65c49
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2021-03-06 12:34:31 -0500

    made axios get request in App.js and Some body styling in css

--- learning-frontend/src/App.js
+++ learning-frontend/src/App.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import CardContainer from './components/CardContainer'
+import axios from 'axios'
 
 
 function App() {
 
+  const SAMPLE_CARDS = []
+
   const [cards, setCards] = useState(SAMPLE_CARDS)
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3000/questions')
+    .then(response => {
+      setCards(response.data.map((cardData, index) => {
+        return {
+          id: `${index}`,
+          subject_id: cardData.subject_id,
+          question: cardData.question,
+          answer: cardData.answer,
+          image: cardData.image
+        }
+      }))
+      console.log(response.data)
+    })
+  }, [])
   
   return (
     <div>
@@ -16,7 +36,7 @@ function App() {
     <Navbar />
     
     </header>
-      <h1>App</h1>
+      {/* <h1>App</h1> */}
       
       <CardContainer cards={cards}/>
       
@@ -24,26 +44,26 @@ function App() {
     </div>
   )
 }
+export default App;
 // }
 
-const SAMPLE_CARDS = [
-{
-  "id": 8,
-  "subject_id": 20,
-  "question": "What is this color?",
-  "answer": "Red",
-  "image": "https://i.pinimg.com/564x/6b/81/52/6b815209f2b5f67eb1a693e32a64575a.jpg"
-},
-{
-  "id": 9,
-  "subject_id": 21,
-  "question": "2 + 2",
-  "answer": "4",
-  "image": ""
-}
-]
+// const SAMPLE_CARDS = [
+// {
+//   "id": 8,
+//   "subject_id": 20,
+//   "question": "What is this color?",
+//   "answer": "Red",
+//   "image": "https://i.pinimg.com/564x/6b/81/52/6b815209f2b5f67eb1a693e32a64575a.jpg"
+// },
+// {
+//   "id": 9,
+//   "subject_id": 21,
+//   "question": "2 + 2",
+//   "answer": "4",
+//   "image": ""
+// }
+// ]
 
-export default App;
   // state = {
   //   ages: [],
   //   subjects: [],
